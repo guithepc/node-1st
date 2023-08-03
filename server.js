814a688e2b40ccea75dfae3c29cb3becbd1a891c
Guilherme <guilherme.pc@yahoo.com>
2023-08-02 23:18:45 -0300

    adding routes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const http = require("http")
 
 const port = 3000;
+
+const rotas = {
+    '/': 'Node start.',
+    '/books': 'Books page',
+    '/authors': 'Authors page',
+    '/publishers': 'Publishers page',
+    '/infos': 'About the project',
+
+}
+
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Node start.')
+    res.end(rotas[req.url])
 })
 
 server.listen(port, () => {
